e43a293216905fe431a812eca88f80472ee10ae2
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2017-11-05 10:15:52 +0000

    Add fragments for each section

---
+++ app/src/main/java/com/rafhaanshah/studyassistant/FlashCardFragment.java
@@ -0,0 +1,25 @@
+package com.rafhaanshah.studyassistant;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FlashCardFragment extends Fragment {
+    public static FlashCardFragment newInstance() {
+        FlashCardFragment fragment = new FlashCardFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_flash_card, container, false);
+    }
+}
---
+++ app/src/main/java/com/rafhaanshah/studyassistant/LectureFragment.java
@@ -0,0 +1,25 @@
+package com.rafhaanshah.studyassistant;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LectureFragment extends Fragment {
+    public static LectureFragment newInstance() {
+        LectureFragment fragment = new LectureFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_lecture, container, false);
+    }
+}
